4d20488e7aa6b9b208b82bd239217e5d9c0c4027
Anson Kao <anson.kao@gmail.com>
2023-11-21 14:10:50 -0500

    Added __init__.py for ComfyUI installation

---
+++ __init__.py
@@ -0,0 +1,35 @@
+"""
+@author: Anson Kao
+@title: ComfyUI Geometry
+@nickname: ComfyUI Geometry
+@description: A small collection of custom nodes for use with ComfyUI, for geometry calculations
+"""
+
+import importlib
+
+print(f"### Loading: ComfyUI Geometry")
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+NODE_MODULES = [
+    ".comfyui_mask_centroid",
+    ".comfyui_mask_eigenvector",
+]
+
+def load_nodes(module_name: str):
+    global NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+    module = importlib.import_module(module_name, package=__name__)
+
+    NODE_CLASS_MAPPINGS = {
+        **NODE_CLASS_MAPPINGS,
+        **module.NODE_CLASS_MAPPINGS,
+    }
+    NODE_DISPLAY_NAME_MAPPINGS = {
+        **NODE_DISPLAY_NAME_MAPPINGS,
+        **module.NODE_DISPLAY_NAME_MAPPINGS,
+    }
+
+for module_name in NODE_MODULES:
+    load_nodes(module_name)
