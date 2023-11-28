92f6879b92826bcca077102ce509c495d349a967
Anson Kao <ansonkao@Ansons-MacBook-Pro.local>
2023-11-28 16:19:05 -0500

    Transform now working

--- comfyui_geometry_nodes.py
+++ comfyui_geometry_nodes.py
@@ -64,7 +64,7 @@ class TransformImageToMatchMask:
 
         return image_tensor
 
-    def calculate_transformation_matrix(self, mask, image):
+    def calculate_transformation(self, mask, image):
         # Convert the tensor to a numpy array and remove the batch and color dimensions
         # The resulting array will have shape [height, width]
         np_image = mask.squeeze().numpy()
@@ -76,8 +76,8 @@ class TransformImageToMatchMask:
         # Find the indices of non-zero (white) pixels
         y_indices, x_indices = np.nonzero(binary_image)
 
-        # Calculate the mean of the points
-        x_mean, y_mean = np.mean(x_indices), np.mean(y_indices)
+        # Calculate the centroid
+        centroid_x, centroid_y = int(np.mean(x_indices)), int(np.mean(y_indices))
 
         # Calculate the covariance matrix
         cov_matrix = np.cov(x_indices, y_indices)
@@ -90,109 +90,78 @@ class TransformImageToMatchMask:
         minor_axis_length = 2 * np.sqrt(eigenvalues[0]) * 2  # Scale as needed
 
         # Angle between x-axis and the major axis of the ellipse in degrees
-        angle = np.arctan2(eigenvectors[1, 1], eigenvectors[0, 1]) * (180 / np.pi)
+        rotation = np.arctan2(eigenvectors[1, 1], eigenvectors[0, 1])
 
         # Draw the ellipse
-        center = (int(x_mean), int(y_mean))
-        axes = (int(major_axis_length / 2), int(minor_axis_length / 2))
+        ellipse_length = int(major_axis_length / 2)
+        ellipse_width = int(minor_axis_length / 2)
 
-        return (center, axes, angle)
+        # Starting and ending dimensions
+        height_out, width_out = mask.shape[1:3]
+        height_in, width_in = image.shape[1:3]
 
-        # # Adjust scale for the identity oval dimensions (using golden ratio)
-        # scale_x = scale_x / 1.618033988749895
-        # scale_y = scale_y / 1
+        # Calculate the scale relative to the original template size
+        scale_x = ellipse_width / 72
+        scale_y = ellipse_length / 100
 
-        # Translation
-        height, width = mask.shape[1:3]
-        translate_x = centroid_x - 0.5 * width
-        translate_y = centroid_y - 0.5 * height
+        # Center of the image
+        cx, cy = int(0.5 * width_out), int(0.5 * height_out)
+
+        # Translation from center of the image
+        translate_x = centroid_x - cx
+        translate_y = centroid_y - cy
         print(f"Centroid: {(centroid_x, centroid_y)}")
         print(f"Translate: {(translate_x, translate_y)}")
 
-        # Center of the image
-        cx, cy = width // 2, height // 2
 
-        # Translate to origin matrix (3x3)
-        translate_to_origin = np.array([
+        # Initial Translation matrix (to origin)
+        initial_translation_matrix = np.array([
             [1, 0, -cx],
             [0, 1, -cy],
             [0, 0, 1]
         ])
 
-        # Translate back to center matrix (3x3)
-        translate_back_to_center = np.array([
-            [1, 0, cx],
-            [0, 1, cy],
-            [0, 0, 1]
-        ])
-
-        # 1. Scaling Matrix (3x3)
-        FACTOR = 2 # Constant to adjust the scaling to match the edges of the mask
+        # Scaling matrix
         scaling_matrix = np.array([
-            [np.sqrt(eigenvalues[0]) * FACTOR / 72, 0, 0],
-            [0, np.sqrt(eigenvalues[1]) * FACTOR / 100, 0],
+            [scale_x, 0, 0],
+            [0, scale_y, 0],
             [0, 0, 1]
         ])
-        scaling_matrix = np.dot(translate_back_to_center, np.dot(scaling_matrix, translate_to_origin))
-        print(f"Scaling Matrix = \n{scaling_matrix}")
 
-        # 2. Rotation Matrix (3x3)
-        # angle = np.arctan2(eigenvectors[1, 0], eigenvectors[0, 0])
-        angle = rotation - np.pi / 2
-        cos, sin = np.cos(angle), np.sin(angle)
+        # Rotation matrix
+        adjusted_rotation = rotation - np.pi / 2 # For whatever reason, the original is off by 90deg
         rotation_matrix = np.array([
-            [cos, -sin, 0],
-            [sin, cos, 0],
+            [np.cos(adjusted_rotation), -np.sin(adjusted_rotation), 0],
+            [np.sin(adjusted_rotation), np.cos(adjusted_rotation), 0],
             [0, 0, 1]
         ])
-        rotation_matrix = np.dot(translate_back_to_center, np.dot(rotation_matrix, translate_to_origin))
-        print(f"Rotation Matrix = \n{rotation_matrix}")
 
-        # 3. Combined Matrix (2x3)
-        combined_matrix = np.dot(scaling_matrix, rotation_matrix)[:2]
+        # Final Translation matrix (to new location)
+        final_translation_matrix = np.array([
+            [1, 0, centroid_x],
+            [0, 1, centroid_y],
+            [0, 0, 1]
+        ])
 
-        return combined_matrix
+        # Combine the matrices
+        matrix = np.dot(final_translation_matrix, np.dot(rotation_matrix, np.dot(scaling_matrix, initial_translation_matrix)))
 
-            # # Translate to origin
-            # translate_to_origin = np.array([
-            #     [1, 0, -width / 2],
-            #     [0, 1, -height / 2],
-            #     [0, 0, 1]
-            # ])
+        print(f"Centroid: {(centroid_x, centroid_y)}")
+        print(f"Ellipse: {(ellipse_length, ellipse_width)}")
+        print(f"Rotation: {rotation}")
+        print(f"Matrix: \n{matrix}")
 
-            # # Scale the eigenvectors
-            # scaled_eigenvector1 = eigenvectors[:, 0] * np.sqrt(eigenvalues[0]) / 144
-            # scaled_eigenvector2 = eigenvectors[:, 1] * np.sqrt(eigenvalues[1]) / 200
-
-            # # Form the combined rotation and scaling matrix
-            # rotation_scaling_matrix = np.column_stack((scaled_eigenvector1, scaled_eigenvector2, [0, 0]))
-            # rotation_scaling_matrix = np.vstack([rotation_scaling_matrix, [0, 0, 1]])
-
-            # # Combine the transformations
-            # combined_transform = np.dot(np.linalg.inv(translate_to_origin), rotation_scaling_matrix)
-            # combined_transform = np.dot(combined_transform, translate_to_origin)
-
-            # # The final matrix to use in cv2.warpAffine should exclude the last row
-            # final_transform_matrix = combined_transform[:2, :]
-
-            # return final_transform_matrix
-
-        # # Scale the eigenvectors (incorporating scaling into rotation)
-        # scaled_eigenvector1 = eigenvectors[:, 0] * np.sqrt(eigenvalues[0]) / 144
-        # scaled_eigenvector2 = eigenvectors[:, 1] * np.sqrt(eigenvalues[1]) / 200
-
-        # # Form the combined rotation and scaling matrix
-        # rotation_scaling_matrix = np.column_stack((scaled_eigenvector1, scaled_eigenvector2))
-
-        # # Create the final transformation matrix
-        # transformation_matrix = np.identity(3)
-        # # transformation_matrix[:2, :2] = rotation_scaling_matrix
-        # transformation_matrix[:2, 2] = [translate_x, translate_y]
-
-        # return transformation_matrix
+        return (
+            centroid_x,
+            centroid_y,
+            ellipse_length,
+            ellipse_width,
+            rotation,
+            matrix,
+        )
 
     def transform_image(self, mask, image):
-        center, axes, angle = self.calculate_transformation_matrix(mask, image)
+        centroid_x, centroid_y, ellipse_length, ellipse_width, rotation, matrix = self.calculate_transformation(mask, image)
 
         # Create a blank canvas the same size as the mask
         height_in, width_in = image.shape[1:3]
@@ -202,25 +171,28 @@ class TransformImageToMatchMask:
         canvas = np.zeros((height_out, width_out, 3), np.uint8)
         print(f"canvas.shape(): {canvas.shape} image.shape(): {image.shape}")
 
-        # # Drop the image into the center of this canvas
-        # x_offset = (width_out - width_in) // 2
-        # y_offset = (height_out - height_in) // 2
-        # image_np = self.convert_image_from_tensor_to_numpy(image)
-        # canvas[y_offset:y_offset + height_in, x_offset:x_offset + width_in] = image_np
+        # Drop the image into the center of this canvas
+        x_offset = (width_out - width_in) // 2
+        y_offset = (height_out - height_in) // 2
+        image_np = self.convert_image_from_tensor_to_numpy(image)
+        canvas[y_offset:y_offset + height_in, x_offset:x_offset + width_in] = image_np
 
 
-        # print(f"Transform Matrix = \n{transformation_matrix}")
+        print(f"Matrix = \n{matrix}")
 
 
 
         # Apply the affine transformation
-        # transformed_image = cv2.warpAffine(canvas, transformation_matrix[:2], (width_out, height_out))
+        transformed_image = cv2.warpAffine(canvas, matrix[:2], (width_out, height_out))
 
-        cv2.ellipse(canvas, center, axes, angle, 0, 360, (0, 255, 0), 2)        
+        centroid = (centroid_x, centroid_y)
+        axes = (ellipse_length, ellipse_width)
+        angle = rotation * (180 / np.pi)
+        cv2.ellipse(transformed_image, centroid, axes, angle, 0, 360, (0, 255, 0), 2)        
 
 
         # print(f"Centroid = {(centroid_x, centroid_y)}")
         # cv2.circle(preview_image, (centroid_x, centroid_y), radius=5, color=(0, 0, 255), thickness=-5)
         # transformed_image = self.convert_image_from_numpy_to_tensor(transformed_image)
-        transformed_image = self.convert_image_from_numpy_to_tensor(canvas)
-        return (transformed_image,)
+        final_image = self.convert_image_from_numpy_to_tensor(transformed_image)
+        return (final_image,)
